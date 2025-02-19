ae0c3e2e3c618d74e7cdf28fc5f4dafb48217406
Akshaya Srinivas <akshaya.pra12@gmail.com>
2025-02-19 20:04:07 +0100

    solved Extra lab

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // Write code that prints out to the console the index of the character “j” in
 
 const string1 = "My favorite dessert is jello";
-
+const result=string1.indexOf("j");
+console.log(result);
 // Your code here...
 
 
@@ -16,6 +17,8 @@ const string1 = "My favorite dessert is jello";
 // Make a new string with the text "COOL" by using only the characters available in the provided string and the bracket notation
 
 const string2 = "ABCDEFGHJKLO";
+const newResult=string2[2]+string2[11]+string2[11]+string2[10];
+console.log(newResult);
 
 // Your code here...
 
@@ -30,7 +33,8 @@ const string2 = "ABCDEFGHJKLO";
 const string3 = "Na";
 
 // Your code here...
-
+const newString=string3.repeat(3);
+console.log(`${newString} Batman`);
 
 
 
@@ -40,7 +44,10 @@ const string3 = "Na";
 // Using the string method .slice(), access and print to the console the name of your favorite fruit from a given string
 
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
-
+const fruitResult=fruit.indexOf("lemon");
+console.log(fruitResult);
+const result2=fruit.slice(fruitResult,fruitResult + 5);
+console.log(result2);
 // Your code here...
 
 
@@ -59,10 +66,18 @@ const funnyHeadline2 = "Students Cook & Serve Grandparents";
 // Check the first headline
 // Your code here ...
 
-
+if(funnyHeadline1.includes("oxygen")){
+    console.log("The string includes the word oxygen");
+}else{
+    console.log("The string does not include the word 'oxygen'")
+}
 // Check the second headline
 // Your code here ...
-
+if(funnyHeadline2.includes("oxygen")){
+    console.log("The string includes the word 'oxygen'");
+}else{
+    console.log("The string does not include the word 'oxygen'")
+}
 
 
 /*******************************************
@@ -75,7 +90,13 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 
 // a) Print the string length
 // Your code here ...
-
+const length=string4.length;
+console.log(length);
 
 // b) Print the last character in the string
 // Your code here ...
+for(let i=0;i<string4.length;i++){
+
+}
+const length2=string4[string4.length-1];
+    console.log(length2);
